f54f1f068df291292792fed40da7b4f07625ba38
Benjamin Milde <benni@kobrakai.de>
2018-01-06 14:47:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Fuelsystem
+
+Fuelsystem is a filesystem abstraction library inspired by [flysystem](https://github.com/thephpleague/flysystem).
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `fuelsystem` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:fuelsystem, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/fuelsystem](https://hexdocs.pm/fuelsystem).
+
---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule Fuelsystem.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :fuelsystem,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env)
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false}
+    ]
+  end
+end
